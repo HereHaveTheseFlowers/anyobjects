b108c28af3df6a2e09227a4df900801093eaa02f
herehavetheseflowers <herehavetheseflowers@gmail.com>
2022-11-09 14:39:47 +0400

    Makes logo navigate to home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { Button } from './../Button';
 import { Anchor } from './../Anchor';
 import bigLogo from '../../assets/svg/logo-big.svg';
 import smallLogo from '../../assets/svg/logo-small.svg';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { RouterList } from '../../router/routerList';
 
@@ -18,13 +19,23 @@ export function Header(props: HeaderProps) {
     }
     const navigate = useNavigate();
     const navigateAbout = () => navigate(RouterList.ABOUT);
+    const navigateHome = () => navigate(RouterList.HOME);
+    useEffect(() => {
+        if(logoSize !== 'small') return;
+        const logo = document.querySelector('.header__logo');
+        if(logo) {
+            logo.addEventListener('click', () => {
+                navigateHome();
+            })
+        }
+    });
     return (
     <header className="header">
         <div className="header__menu header__menu_left">
             <Button onClick={navigateAbout} className={buttonAboutClass}>О НАС</Button>
             <Anchor className="header__menu-anchor" href="https://www.instagram.com/normcultura/">ИНСТАГРАМ</Anchor>
         </div>
-        <img src={logoSize === 'big' ? bigLogo : smallLogo} className={`header__logo_${logoSize}`} alt='Логотип а ну' draggable='false' />
+        <img src={logoSize === 'big' ? bigLogo : smallLogo} className={`header__logo header__logo_${logoSize}`} alt='Логотип а ну' draggable='false' />
         <div className="header__menu header__menu_right">
             <Button>ПРЕДЛОЖИТЬ ОБЪЕКТ</Button>
         </div>
