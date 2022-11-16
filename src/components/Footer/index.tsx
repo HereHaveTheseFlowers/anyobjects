f6376c2dced4c0719626e0b1c98b5275068b3712
herehavetheseflowers <herehavetheseflowers@gmail.com>
2022-11-16 21:12:19 +0400

    Fixes ∀

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { Anchor } from './../Anchor';
 export function Footer() {
   return (
     <footer className="footer">
-        <span>∀ НУ, 2022</span>
+        <span><span className="∀-fix">∀</span> НУ, 2022</span>
         <span>ДИЗАЙН КОНЦЕПЦИЯ: <Anchor href={'http://normcultura.agency'}>НОРМ КУЛЬТУРА</Anchor></span>
         <span>РАЗРАБОТКА: <Anchor href={'https://github.com/HereHaveTheseFlowers'}>ДЕРЖИ ЦВЕТЫ</Anchor></span>
     </footer>
