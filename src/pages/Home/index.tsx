e53bb2a1937942d645140980bfe30a59585c4090
herehavetheseflowers <herehavetheseflowers@gmail.com>
2022-12-02 17:33:45 +0400

    scroller tweak

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,27 @@
 import { Footer, Header, Button, FiltersTab, ObjectGrid } from '../../components';
+import { useRef, useEffect } from 'react';
 
 export default function Home() {
+    const arrowRef = useRef(null);
 
     let lastScrollTimer = 0;
-    window.onscroll = function() {
-        lastScrollTimer++;
-        if(lastScrollTimer > 10) {
-            lastScrollTimer = 0;
-            const distanceScrolled = document.documentElement.scrollTop;
-            if (distanceScrolled > 30) {
-                const arrow = document.querySelector('.home__arrow') as HTMLElement;
-                if(arrow) arrow.style.opacity = '1';
-            } else {
-                const arrow = document.querySelector('.home__arrow') as HTMLElement;
-                if(arrow) arrow.style.opacity = '0';
+    useEffect(() => {
+        window.onscroll = function() {
+            lastScrollTimer++;
+            if(lastScrollTimer > 10) {
+                lastScrollTimer = 0;
+                const distanceScrolled = document.documentElement.scrollTop;
+                if (distanceScrolled > 30) {
+                    if(arrowRef && arrowRef.current) arrowRef.current.style.opacity = '1';
+                } else {
+                    if(arrowRef && arrowRef.current) arrowRef.current.style.opacity = '0';
+                }
             }
         }
-    }
+        return () => {
+            window.onscroll = null;
+        }
+    }, [])
 
     const handleArrow = () => {
         window.scrollTo(0, 0);
@@ -31,7 +36,7 @@ export default function Home() {
             <FiltersTab />
             <ObjectGrid />
             <Footer />
-            <button className="home__arrow" onClick={handleArrow} aria-label="Иконка стрелочки вверх">
+            <button className="home__arrow" onClick={handleArrow} aria-label="Иконка стрелочки вверх" ref={arrowRef}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="30">
                     <path d="m8 .96 7.19 7.19-1.56 1.55L9.2 5.29v23.75H6.8V5.29l-4.39 4.4-1.6-1.57L8 .96Z"/>
                 </svg>
